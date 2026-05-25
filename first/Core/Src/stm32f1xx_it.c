3e7dbeb20faf6b76d0e7649fea5cfc55012ef358
SEUNGCHANPARK\seung <seungchan141414@gmail.com>
2026-05-25 19:55:18 +0900

    10000만번의 클럭이 흐르면 Counter 1이 올라간다 Counter갸 800번이 되면 0으로 돌아간다. 0~799 -> 0으로 돌아가는 거리는 시간은 1초
    STM특성 우리의 클럭은 8Mhz이다.
    8,000,000 마다 클럭이 올라간다

--- first/Core/Src/stm32f1xx_it.c
+++ first/Core/Src/stm32f1xx_it.c
@@ -42,6 +42,7 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN PV */
+static uint32_t m_time = 0;
 
 /* USER CODE END PV */
 
@@ -205,6 +206,7 @@ void SysTick_Handler(void)
 void TIM2_IRQHandler(void)
 {
   /* USER CODE BEGIN TIM2_IRQn 0 */
+	m_time++;
 
   /* USER CODE END TIM2_IRQn 0 */
   HAL_TIM_IRQHandler(&htim2);
